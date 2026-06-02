fee664f944f8fb8f982e6086fefdefcb48fd6de1
Mr-developer-007 <vivek.pundir.dev@gmail.com>
2026-06-02 16:05:53 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -28,7 +28,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-    origin:process.env.FRONTEND_URL?.split(","),
+    origin:process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials:true
 }))
